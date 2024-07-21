b04690951c140f0b8a71421be5acb90e9413f999
ilsu-sunal <sunalilsu@gmail.com>
2024-07-21 22:32:06 +0300

    Context and connections

--- FoodOrderApp/Data/Concrete/EfCore/AppDbContext.cs
+++ FoodOrderApp/Data/Concrete/EfCore/AppDbContext.cs
@@ -1,7 +1,20 @@
+using FoodOrderApp.Entity;
+using Microsoft.EntityFrameworkCore;
+
 namespace FoodOrderApp.Data.Concrete.EfCore
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
-        
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options){}
+        public DbSet<User> Users => Set<User>();
+        public DbSet<Card> Cards => Set<Card>();
+        public DbSet<Address> Addresses => Set<Address>();
+        public DbSet<Order> Orders => Set<Order>();
+        public DbSet<Product> Products => Set<Product>();
+        public DbSet<ProductVar> ProductVars => Set<ProductVar>();
+        public DbSet<Material> Materials => Set<Material>();
+        public DbSet<Category> Categories => Set<Category>();
+        public DbSet<OrderProduct> OrderProducts => Set<OrderProduct>();
+        public DbSet<Payment> Payments => Set<Payment>();
     }
 }
--- FoodOrderApp/Program.cs
+++ FoodOrderApp/Program.cs
@@ -1,4 +1,14 @@
+using FoodOrderApp.Data.Concrete.EfCore;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options => {
+    var config = builder.Configuration;
+    var connectionString = config.GetConnectionString("sql_connection");
+    options.UseSqlite(connectionString);
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
