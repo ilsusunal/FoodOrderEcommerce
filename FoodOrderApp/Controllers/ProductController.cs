3e26e3d72feacf56df313dffac08882494784b75
ilsu-sunal <sunalilsu@gmail.com>
2024-07-23 00:32:15 +0300

    Views folder created

--- FoodOrderApp/Controllers/ProductController.cs
+++ FoodOrderApp/Controllers/ProductController.cs
@@ -1,15 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using FoodOrderApp.Data.Concrete.EfCore;
-using FoodOrderApp.Entity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FoodOrderApp.Controllers
 {
     public class ProductController : Controller
     {
-
+        public IActionResult Index(){
+            return View();
+        }
     }
 }
