fc8614083d21fbb2ef814f1ad8e97c20ac972854
ilsu-sunal <sunalilsu@gmail.com>
2024-07-25 23:34:34 +0300

    User update

--- FoodOrderApp/Entity/User.cs
+++ FoodOrderApp/Entity/User.cs
@@ -1,12 +1,10 @@
+using Microsoft.AspNetCore.Identity;
+
 namespace FoodOrderApp.Entity
 {
-    public class User
+    public class User : IdentityUser
     {
-        public int UserId { get; set; }
         public string? FullName { get; set; }
-        public string? Email { get; set; }
-        public string? Password { get; set; }
-        public int PhoneNumber { get; set; }
         public List<UserCard> UserCards { get; set; } = new List<UserCard>();
         public List<UserAddress> UserAddresses { get; set; } = new List<UserAddress>();
         public List<ShoppingCart> Orders{ get; set; }= new List<ShoppingCart>();
